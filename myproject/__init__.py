9eea7d4fe8cbc10f3c47591795877da59a8730f6
George Higbie <ghigbie@monigle.com>
2019-04-03 14:19:04 -0600

    Edit init

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -5,8 +5,10 @@ from flask_migrate import flask_migrate
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'myscretkey'
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'= os.path.join(basedir)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir)
 app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 
 db = SQLAlchemy(app)
