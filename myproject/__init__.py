3a3a4a325a712c9ca03ee2d758f4774d270c5359
George Higbie <ghigbie@monigle.com>
2019-04-03 14:18:03 -0600

    Add init.py  file

---
+++ myproject/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import flask_migrate
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'= os.path.join(basedir)
+app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
