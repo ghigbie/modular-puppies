b922d6163eeb6d92447df63020513b86fc19d185
George Higbie <ghigbie@monigle.com>
2019-04-05 17:16:20 -0600

    Change views2 to views for owners

--- myproject/owners/views.py
+++ myproject/owners/views.py
@@ -5,16 +5,14 @@ from myproject.owners.froms import AddForm
 
 owners_blueprint = Blueprint('owners', __name__, template_folder='templates/owners')
 
+@owners_blueprint.route('/add', methos=['GET', 'POST'])
 def add():
     form = AddForm
-    if form.validate_on_submit():
-            name = form.name.data
-            pup_id = form.pup_id.data
-            new_owner = Owner(name, pup_id)
-            db.session.add(new_owner)
-            db.session.commit()
-            return redirect(url_for('puppies.list'))
-return render_template('add_owner.html', form=form)
-
-
-
+    if form.valid_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+        return redirect(url_for('puppies.list'))
+    return render_template('add_owner.html', form=form)
