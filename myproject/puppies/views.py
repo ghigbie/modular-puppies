acad17b2393ec38bab67b7e0d8b3ab2806883764
George Higbie <ghigbie@monigle.com>
2019-04-05 17:21:40 -0600

    Add views for owner

--- myproject/puppies/views.py
+++ myproject/puppies/views.py
@@ -2,3 +2,18 @@ from flask import Blueprint, render_template, redirect, url_for
 from myproject import db
 from myproject.models import Puppy
 from myproject.puppies.forms import AddForm, DelForm
+
+puppies_blueprint = Blueprint('puppies', __name__, template_folder='templates/puppies')
+
+def add():
+    form = AddForm
+    if form.valid_on_submit():
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+        return redirect(url_for('puppies.list'))
+    return render_template('add.html', form=form)
+
+def list():
+    
