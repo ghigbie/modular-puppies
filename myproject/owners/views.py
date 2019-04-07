18857331e9ce2706393c5b544046db6728248c32
ghigbie <gehigbie@gmail.com>
2019-04-07 08:24:47 -0600

    Use owners bluprint to route

---
+++ myproject/owners/views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, redirect, url_for
+from myproject import db 
+from myproject.models import Owner 
+from myproject.owners.forms import AddForm
+
+owners_bluprint = Blueprint('owners', __name__, template_folder, templates=owners)
+
+@owners_blueprint.route('/add', mehtods=['GET', 'POST'])
+def add():
+
+    form = AddForm()
+    if form.valid_on_submit():
+        name = form.name.data
+        pup_id form.pup_id.data
+        new_owner = Owner(name, pup_id)
+        db.sesion.add(new_owner)
+        db.sesion.commit()
+
+        return redirect(url_for('puppies.list')
+    return render_template('add_owner.html', form=form)
