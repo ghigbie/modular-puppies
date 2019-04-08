7065eb3a8961dbc10b821b79b8f36ade66f13785
ghigbie <gehigbie@gmail.com>
2019-04-07 18:41:33 -0600

    Edit spelling

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'myscretkey'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir)
-app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
